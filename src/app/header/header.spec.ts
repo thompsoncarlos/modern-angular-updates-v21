c45adba932817ad13359a8a87c320e104f7adc93
carlosthompson <carlosethompson@gmail.com>
2026-02-09 16:28:08 +0000

    add header component

--- src/app/header/header.spec.ts
+++ src/app/header/header.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Hello } from './hello';
+import { Header } from './header';
 
-describe('Hello', () => {
-  let component: Hello;
-  let fixture: ComponentFixture<Hello>;
+describe('Header', () => {
+  let component: Header;
+  let fixture: ComponentFixture<Header>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Hello]
+      imports: [Header]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Hello);
+    fixture = TestBed.createComponent(Header);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
