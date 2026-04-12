aedc44a833ebc89a6f985873b26ebdaa21094df9
carlosthompson <carlosethompson@gmail.com>
2026-04-12 22:17:19 +0100

    feat: update import angular material and call app selectors

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
